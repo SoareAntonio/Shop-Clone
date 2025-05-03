84730f3d6894949489480e0877634a1e069d236d
Antonio <antonio.soare.42005@gmail.com>
2025-05-03 23:36:11 +0300

    Update cartTest

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -3,8 +3,12 @@ import { deliveryOptions } from "../../data/deliveryOptions.js";
 
 describe('test suite:addToCart', () => {
 
-  it('adds an existing product to the cart', () => {
-    spyOn(localStorage, 'setItem');
+  // it('adds an existing product to the cart', () => {
+    beforeEach(() => {
+      spyOn(localStorage, 'setItem');
+    });
+  
+    it('adds an existing product to the cart', () => {
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([
         {
@@ -21,12 +25,17 @@ describe('test suite:addToCart', () => {
 
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }]));
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(2); // a crescut cantitatea
   });
   
   it('adds a new product to the cart', () => {
-    spyOn(localStorage, 'setItem');
+    
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]); // coș gol
     });
@@ -38,6 +47,11 @@ describe('test suite:addToCart', () => {
 
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '1'
+    }]));
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(2);
   });
