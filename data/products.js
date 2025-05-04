1c59918098cc95627d79f461ac9fc0592cbfdd37
Antonio <antonio.soare.42005@gmail.com>
2025-05-04 10:20:00 +0300

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
 export function getProduct(productId){
   let matchingProduct;
     products.forEach((product)=>{
@@ -6,6 +8,28 @@ export function getProduct(productId){
       }
     });
     return matchingProduct;
+}
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){
+    this.id=productDetails.id;
+    this.image=productDetails.image;
+    this.name=productDetails.name;
+    this.rating=productDetails.rating;
+    this.priceCents=productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars*10}.png`;
+  }
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
 }
 export const products = [
   {
@@ -698,4 +722,7 @@ export const products = [
       "water"
     ]
   }
-];
+].map((productDetails)=>{
+  return new Product(productDetails);
+  //map take each value ,run function transform and put it inside new array
+});
