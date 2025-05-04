17b30359b4ca755b57622945f624612aa65ccb2b
Antonio <antonio.soare.42005@gmail.com>
2025-05-04 16:41:28 +0300

    Update some product with instruction &warnings

---
+++ data/car.js
@@ -0,0 +1,85 @@
+class Car{
+  #brand;
+  #model;
+  speed=0;
+  isTrunkOpen=false;
+  constructor (carDetails){
+    this.#brand=carDetails.brand;
+    this.#model=carDetails.model;
+  }
+  displayInfo(){
+    const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
+    console.log(
+      `${this.#brand} ${this.#model}, Speed: ${this.speed} km/h, Trunk: ${trunkStatus}`
+    );
+  }
+  go() {
+    if (!this.isTrunkOpen) {
+      this.speed += 5;
+    }
+    // Limit the speed to 200.
+    if (this.speed > 200) {
+      this.speed = 200;
+    }
+  }
+
+  brake() {
+    this.speed -= 5;
+
+    // Limit the speed to 0.
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+  }
+  openTrunk(){
+    if(this.speed===0){
+      this.isTrunkOpen=true;
+    }
+  }
+  closeTrunk(){
+    this.isTrunkOpen=false;
+  }
+}
+
+class RaceCar extends Car{
+  acceleration;
+  constructor(carDetails) {
+    super(carDetails);
+    this.acceleration = carDetails.acceleration;
+  }
+
+  go() {
+    this.speed += this.acceleration;
+
+    if (this.speed > 300) {
+      this.speed = 300;
+    }
+  }
+
+  openTrunk() {
+    console.log('Race cars do not have a trunk.');
+  }
+
+  closeTrunk() {
+    console.log('Race cars do not have a trunk.');
+  }
+}
+
+const car1=new Car({
+  brand:'Toyota',
+  model:'Corolla'
+});
+
+const raceCar = new RaceCar({
+  brand: 'McLaren',
+  model: 'F1',
+  acceleration: 20
+});
+raceCar.go();
+raceCar.go();
+raceCar.go();
+raceCar.displayInfo();
+raceCar.openTrunk();
+raceCar.displayInfo();
+raceCar.brake();
+raceCar.displayInfo();
--- data/products.js
+++ data/products.js
@@ -50,8 +50,31 @@ class Clothing extends Product{
     </a>
     `;
   }
-
 }
+
+class Appliance extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.instructionsLink}" target="_blank">
+        Instructions
+      </a>
+      <a href="${this.warrantyLink}" target="_blank">
+        Warranty
+      </a>
+    `;
+  }
+}
+
+
 
 export const products = [
   {
@@ -113,7 +136,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type:'appliance',
+    instructionsLink:'images/appliance-instructions.png',
+    warrantyLink:'images/appliance-warranty.png'
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -298,7 +324,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type:'appliance',
+    instructionsLink:'images/appliance-instructions.png',
+    warrantyLink:'images/appliance-warranty.png'
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -603,7 +632,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type:'appliance',
+    instructionsLink:'images/appliance-instructions.png',
+    warrantyLink:'images/appliance-warranty.png'
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -663,7 +695,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type:'appliance',
+    instructionsLink:'images/appliance-instructions.png',
+    warrantyLink:'images/appliance-warranty.png'
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -747,6 +782,8 @@ export const products = [
 ].map((productDetails)=>{
   if(productDetails.type==='clothing'){
     return new Clothing(productDetails);
+  } else if (productDetails.type === 'appliance') {
+    return new Appliance(productDetails);
   }
   return new Product(productDetails);
   //map take each value ,run function transform and put it inside new array
